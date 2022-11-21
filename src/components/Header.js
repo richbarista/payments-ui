ffc2457aa45ae1e4f4d2041822e326706b46c20d
Richard Clarke <rclbz@allstate.com>
2022-11-21 10:05:25 +0000

    monday morn

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,12 +7,12 @@ function Header() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home"><img src="https://www.allstate.com/dist/content-client-react-app/static/media/logo-large.cca8060d.svg" width="200" alt="" class="d-inline-block align-middle mr-2" /></Navbar.Brand>
+        <Navbar.Brand href="/"><img src="https://www.allstate.com/dist/content-client-react-app/static/media/logo-large.cca8060d.svg" width="200" alt="" class="d-inline-block align-middle mr-2" /></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Find a transaction</Nav.Link>
-            <Nav.Link href="#link">New transaction</Nav.Link>
+            <Nav.Link href="/home">Find a transaction</Nav.Link>
+            <Nav.Link href="/link">New transaction</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
